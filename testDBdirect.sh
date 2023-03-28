3cda22260ae5e1c64b00d3352fb77c96d5548de1
Ramesh chinnaraj <110036451+Rameshei87@users.noreply.github.com>
2023-03-29 08:08:48 +1000

    Add files via upload

---
+++ testDBdirect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#LogDirectory='/app/bmobsps/SPS/Trunk'
+#DataDirectory='/var/tmp/data'
+
+#DBUSER='PROV_DEV_BMOB'
+#DBUSERPASSWORD='PROV_DEV_BMOB'
+#DB='SPS_BMOB_DEV'
+
+sqlplus -s SPS_PROD/SPS_PROD@CBS
+
+#select * from m_vendor_ne;
+
+#EOF
+
